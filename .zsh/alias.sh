9b761c3b6144d6637e2d09460e48919768260a0c
Munieru <20086673+munierujp@users.noreply.github.com>
2020-12-30 11:39:46 +0900

    Update alias.sh

--- .zsh/alias.sh
+++ .zsh/alias.sh
@@ -1,2 +1,5 @@
+alias mov1280='(){echo "ffmpeg -i $1 -vf scale=1280:-1 $2";ffmpeg -i $1 -vf scale=1280:-1 $2}'
+alias mov1920='(){echo "ffmpeg -i $1 -vf scale=1920:-1 $2";ffmpeg -i $1 -vf scale=1920:-1 $2}'
+alias mov2048='(){echo "ffmpeg -i $1 -vf scale=2048:-1 $2";ffmpeg -i $1 -vf scale=2048:-1 $2}'
 alias pip='pip3'
 alias python='python3'
