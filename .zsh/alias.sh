7350d5be980766eb2fce65e4285c66173f13f34d
Munieru <20086673+munierujp@users.noreply.github.com>
2021-02-14 02:24:51 +0900

    Update alias.sh

--- .zsh/alias.sh
+++ .zsh/alias.sh
@@ -3,5 +3,3 @@ alias mov1920='(){echo "ffmpeg -i $1 -vf scale=1920:-1 $2";ffmpeg -i $1 -vf scal
 alias mov2048='(){echo "ffmpeg -i $1 -vf scale=2048:-1 $2";ffmpeg -i $1 -vf scale=2048:-1 $2}'
 alias pip='pip3'
 alias python='python3'
-# see https://github.com/h-matsuo/macOS-trash
-alias rm='trash'
