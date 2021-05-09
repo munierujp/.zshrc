9468f935bb0b530b7ecd08770a320c5b50d3972d
Munieru <20086673+munierujp@users.noreply.github.com>
2021-05-09 12:07:46 +0900

    add renamepic

--- .zsh/alias.sh
+++ .zsh/alias.sh
@@ -3,3 +3,4 @@ alias mov1920='(){echo "ffmpeg -i $1 -vf scale=1920:-1 $2";ffmpeg -i $1 -vf scal
 alias mov2048='(){echo "ffmpeg -i $1 -vf scale=2048:-1 $2";ffmpeg -i $1 -vf scale=2048:-1 $2}'
 alias pip='pip3'
 alias python='python3'
+alias renamepic='exiftool "-FileName<CreateDate" -d %Y-%m-%d_%H.%M.%S.%%e'
